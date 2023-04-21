66c4b651bd3159ceded269d2907f71357b2557f1
Mehrael <mehraelashraf20@gmail.com>
2023-04-21 16:17:11 +0200

    first commit

---
+++ [TEMPLATE]/GraphAnalysis/GraphAnalysis.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem
+{
+    public static class GraphAnalysis
+    {
+        #region YOUR CODE IS HERE
+        //Your Code is Here:
+        //==================
+        /// <param name="vertices"></param>
+        /// <param name="edges"></param>
+        /// <param name="startVertex"></param>
+        /// <param name="outputs"></param>
+
+        /// <summary>
+        /// Analyze the edges of the given DIRECTED graph by applying DFS starting from the given "startVertex" and count the occurrence of each type of edges
+        /// NOTE: during search, break ties (if any) by selecting the vertices in ASCENDING alpha-numeric order
+        /// </summary>
+        /// <param name="vertices">array of vertices in the graph</param>
+        /// <param name="edges">array of edges in the graph</param>
+        /// <param name="startVertex">name of the start vertex to begin from it</param>
+        /// <returns>return array of 3 numbers: outputs[0] number of backward edges, outputs[1] number of forward edges, outputs[2] number of cross edges</returns>
+        public static int[] AnalyzeEdges(string[] vertices, KeyValuePair<string, string>[] edges, string startVertex)
+        {
+            //REMOVE THIS LINE BEFORE START CODING
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
